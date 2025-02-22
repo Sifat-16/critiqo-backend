6b2c253c196781c7431ecf25cfffbdab652caace
Ali <ali@Alis-MacBook-Air.local>
2025-02-23 00:44:30 +0600

    Initial Commit

---
+++ src/main/java/com/critiqo/critiqo_backend/CritiqoBackendApplication.java
@@ -0,0 +1,13 @@
+package com.critiqo.critiqo_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CritiqoBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CritiqoBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/critiqo/critiqo_backend/CritiqoBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.critiqo.critiqo_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CritiqoBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
